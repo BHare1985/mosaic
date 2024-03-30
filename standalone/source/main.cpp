a341b4cdeb436332b213ce2e5199b01b2d709cf8
Brian Hare <bhare1985@gmail.com>
2024-03-30 14:28:49 -0400

    Add template

---
+++ standalone/source/main.cpp
@@ -0,0 +1,53 @@
+#include <mosiac/mosiac.h>
+#include <mosiac/version.h>
+
+#include <cxxopts.hpp>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+auto main(int argc, char** argv) -> int {
+  const std::unordered_map<std::string, mosiac::LanguageCode> languages{
+      {"en", mosiac::LanguageCode::EN},
+      {"de", mosiac::LanguageCode::DE},
+      {"es", mosiac::LanguageCode::ES},
+      {"fr", mosiac::LanguageCode::FR},
+  };
+
+  cxxopts::Options options(*argv, "A program to welcome the world!");
+
+  std::string language;
+  std::string name;
+
+  // clang-format off
+  options.add_options()
+    ("h,help", "Show help")
+    ("v,version", "Print the current version number")
+    ("n,name", "Name to greet", cxxopts::value(name)->default_value("World"))
+    ("l,lang", "Language code to use", cxxopts::value(language)->default_value("en"))
+  ;
+  // clang-format on
+
+  auto result = options.parse(argc, argv);
+
+  if (result["help"].as<bool>()) {
+    std::cout << options.help() << std::endl;
+    return 0;
+  }
+
+  if (result["version"].as<bool>()) {
+    std::cout << "Mosiac, version " << MOSIAC_VERSION << std::endl;
+    return 0;
+  }
+
+  auto langIt = languages.find(language);
+  if (langIt == languages.end()) {
+    std::cerr << "unknown language code: " << language << std::endl;
+    return 1;
+  }
+
+  mosiac::Mosiac mosiac(name);
+  std::cout << mosiac.greet(langIt->second) << std::endl;
+
+  return 0;
+}
---
+++ test/source/mosiac.cpp
@@ -0,0 +1,21 @@
+#include <doctest/doctest.h>
+#include <mosiac/mosiac.h>
+#include <mosiac/version.h>
+
+#include <string>
+
+TEST_CASE("Mosiac") {
+  using namespace mosiac;
+
+  Mosiac mosiac("Tests");
+
+  CHECK(mosiac.greet(LanguageCode::EN) == "Hello, Tests!");
+  CHECK(mosiac.greet(LanguageCode::DE) == "Hallo Tests!");
+  CHECK(mosiac.greet(LanguageCode::ES) == "¡Hola Tests!");
+  CHECK(mosiac.greet(LanguageCode::FR) == "Bonjour Tests!");
+}
+
+TEST_CASE("Mosiac version") {
+  static_assert(std::string_view(MOSIAC_VERSION) == std::string_view("1.0"));
+  CHECK(std::string(MOSIAC_VERSION) == std::string("1.0"));
+}
